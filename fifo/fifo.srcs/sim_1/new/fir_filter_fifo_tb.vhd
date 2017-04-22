c103b5b943bda2f1fbd921d62c9b8487ffc931c6
Connor McLaughlin <mclaughc@outlook.com>
2017-04-22 19:40:28 +1000

    Implement fir_filter using join_roundrobin/split_duplicate components

---
+++ fifo/fifo.srcs/sim_1/new/fir_filter_fifo_tb.vhd
@@ -0,0 +1,93 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 22.04.2017 17:39:13
+-- Design Name: 
+-- Module Name: fir_filter_half_fifo_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity fir_filter_fifo_tb is
+end fir_filter_fifo_tb;
+
+architecture Behavioral of fir_filter_fifo_tb is
+
+signal clk : std_logic := '0';
+signal reset : std_logic := '1';
+
+signal fir_filter_input_fifo_data_in : std_logic_vector(31 downto 0);
+signal fir_filter_input_fifo_write_enable : std_logic;
+signal fir_filter_input_fifo_full : std_logic;
+
+signal sink_input_fifo_data_in : std_logic_vector(31 downto 0);
+signal sink_input_fifo_write_enable : std_logic;
+signal sink_input_fifo_full : std_logic;
+
+begin
+
+fir_filter_comp : entity work.fir_filter_fifo(Behavioral)
+    port map (
+        clk => clk,
+        reset => reset,
+        input_fifo_data_in => fir_filter_input_fifo_data_in,
+        input_fifo_write_enable => fir_filter_input_fifo_write_enable,
+        input_fifo_full => fir_filter_input_fifo_full,
+        output_fifo_data_in => sink_input_fifo_data_in,
+        output_fifo_write_enable => sink_input_fifo_write_enable,
+        output_fifo_full => sink_input_fifo_full
+    );
+
+sink_comp : entity work.fifo_sink(Behavioral)
+    generic map (
+        DATA_WIDTH => 32
+    )
+    port map (
+        clk => clk,
+        reset => reset,
+        input_fifo_data_in => sink_input_fifo_data_in,
+        input_fifo_write_enable => sink_input_fifo_write_enable,
+        input_fifo_is_full => sink_input_fifo_full
+    );
+
+clkgen : process is
+begin
+    wait for 0.5ns;
+    clk <= not clk;
+end process;
+
+main : process is
+begin
+    reset <= '1';
+    wait for 1ns;
+    
+    reset <= '0';
+    fir_filter_input_fifo_data_in <= std_logic_vector(to_signed(741343, 32));
+    fir_filter_input_fifo_write_enable <= '1';
+    wait for 1ns;
+    
+    fir_filter_input_fifo_data_in <= std_logic_vector(to_signed(-741343, 32));
+    wait for 1ns;
+       
+    fir_filter_input_fifo_write_enable <= '0';
+    wait for 1ns;
+    
+    wait for 1000sec;
+end process;
+
+
+end Behavioral;
--- fifo/fifo.srcs/sources_1/new/fir_filter_fifo.vhd
+++ fifo/fifo.srcs/sources_1/new/fir_filter_fifo.vhd
@@ -15,64 +15,21 @@ end fir_filter_fifo;
 
 architecture Behavioral of fir_filter_fifo is
 
-signal input_fifo_read_enable : std_logic;
-signal input_fifo_data_out : std_logic_vector(31 downto 0);
-signal input_fifo_empty : std_logic;
-
-signal split_input : std_logic_vector(31 downto 0);
-signal join_input : std_logic_vector(31 downto 0);
-
-signal filter1_in_fifo_write_enable : std_logic;
 signal filter1_in_fifo_data_in : std_logic_vector(31 downto 0);
+signal filter1_in_fifo_write_enable : std_logic;
 signal filter1_in_fifo_full : std_logic;
-signal filter1_out_fifo_read_enable : std_logic;
-signal filter1_out_fifo_write_enable : std_logic;
 signal filter1_out_fifo_data_in : std_logic_vector(31 downto 0);
-signal filter1_out_fifo_data_out : std_logic_vector(31 downto 0);
-signal filter1_out_fifo_empty : std_logic;
+signal filter1_out_fifo_write_enable : std_logic;
 signal filter1_out_fifo_full : std_logic;
 
-signal filter2_in_fifo_write_enable : std_logic;
 signal filter2_in_fifo_data_in : std_logic_vector(31 downto 0);
+signal filter2_in_fifo_write_enable : std_logic;
 signal filter2_in_fifo_full : std_logic;
-signal filter2_out_fifo_read_enable : std_logic;
-signal filter2_out_fifo_write_enable : std_logic;
 signal filter2_out_fifo_data_in : std_logic_vector(31 downto 0);
-signal filter2_out_fifo_data_out : std_logic_vector(31 downto 0);
-signal filter2_out_fifo_empty : std_logic;
+signal filter2_out_fifo_write_enable : std_logic;
 signal filter2_out_fifo_full : std_logic;
 
 begin
-
-input_fifo : entity work.fifo(Behavioral)
-    generic map (
-        DATA_WIDTH => 32
-    )
-    port map (
-        clk => clk,
-        reset => reset,
-        write_enable => input_fifo_write_enable,
-        read_enable => input_fifo_read_enable,
-        data_in => input_fifo_data_in,
-        data_out => input_fifo_data_out,
-        is_empty => input_fifo_empty,
-        is_full => input_fifo_full
-    );
-    
-filter1_fifo : entity work.fifo(Behavioral)
-    generic map (
-        DATA_WIDTH => 32
-    )
-    port map (
-        clk => clk,
-        reset => reset,
-        write_enable => filter1_out_fifo_write_enable,
-        read_enable => filter1_out_fifo_read_enable,
-        data_in => filter1_out_fifo_data_in,
-        data_out => filter1_out_fifo_data_out,
-        is_empty => filter1_out_fifo_empty,
-        is_full => filter1_out_fifo_full
-    );
 
 filter1 : entity work.fir_filter_half_fifo(Behavioral)
     port map (
@@ -84,21 +41,6 @@ filter1 : entity work.fir_filter_half_fifo(Behavioral)
         output_fifo_write_enable => filter1_out_fifo_write_enable,
         output_fifo_data_in => filter1_out_fifo_data_in,
         output_fifo_full => filter1_out_fifo_full
-    );
-    
-filter2_fifo : entity work.fifo(Behavioral)
-    generic map (
-        DATA_WIDTH => 32
-    )
-    port map (
-        clk => clk,
-        reset => reset,
-        write_enable => filter2_out_fifo_write_enable,
-        read_enable => filter2_out_fifo_read_enable,
-        data_in => filter2_out_fifo_data_in,
-        data_out => filter2_out_fifo_data_out,
-        is_empty => filter2_out_fifo_empty,
-        is_full => filter2_out_fifo_full
     );
     
 filter2 : entity work.fir_filter_half_fifo(Behavioral)
@@ -113,72 +55,40 @@ filter2 : entity work.fir_filter_half_fifo(Behavioral)
         output_fifo_full => filter2_out_fifo_full
     );
 
--- TODO: Move to its own component.
-split : process(clk)
-    variable do_write : boolean; -- this could be a signal 
-begin
-    if (rising_edge(clk)) then
-        if (reset = '1') then
-            input_fifo_read_enable <= '0';
-            filter1_in_fifo_write_enable <= '0';
-            filter2_in_fifo_write_enable <= '0';
-        else
-            if (do_write = false and input_fifo_empty = '0' and filter1_in_fifo_full = '0' and filter2_in_fifo_full = '0') then
-                -- Set RE on input_fifo to grab a element
-                -- TODO: Move element register because of fifo behavior
-                input_fifo_read_enable <= '1';
-                split_input <= input_fifo_data_out;
-                do_write := true;
-            end if;
-            
-            -- Pull an element from input_fifo, add it to filter1 and filter2
-            if (do_write) then
-                filter1_in_fifo_write_enable <= '1';
-                filter1_in_fifo_data_in <= split_input;
-                filter2_in_fifo_write_enable <= '1';
-                filter2_in_fifo_data_in <= split_input;
-                do_write := false;
-            else
-                filter1_in_fifo_write_enable <= '0';
-                filter2_in_fifo_write_enable <= '0';
-            end if;
-        end if;
-    end if;
-end process;
+split : entity work.split_duplicate(Behavioral)
+    generic map (
+        FIFO_DATA_WIDTH => 32
+    )
+    port map (
+        clk => clk,
+        reset => reset,
+        input_fifo_write_enable => input_fifo_write_enable,
+        input_fifo_data_in => input_fifo_data_in,
+        input_fifo_full => input_fifo_full,
+        output1_fifo_write_enable => filter1_in_fifo_write_enable,
+        output1_fifo_data_in => filter1_in_fifo_data_in,
+        output1_fifo_full => filter1_in_fifo_full,
+        output2_fifo_write_enable => filter2_in_fifo_write_enable,
+        output2_fifo_data_in => filter2_in_fifo_data_in,
+        output2_fifo_full => filter2_in_fifo_full
+    );
 
--- TODO: Move to its own component.
-join : process(clk)
-    variable read_flipflop : boolean;
-begin
-    if (rising_edge(clk)) then
-        if (reset = '1') then
-            read_flipflop := false;
-            filter1_out_fifo_read_enable <= '0';
-            filter2_out_fifo_read_enable <= '0';
-            output_fifo_write_enable <= '0';
-        else
-            if (read_flipflop = false) then
-                filter2_out_fifo_read_enable <= '0';
-                if (filter1_out_fifo_empty = '0' and output_fifo_full = '0') then
-                    filter1_out_fifo_read_enable <= '1';
-                    output_fifo_write_enable <= '1';
-                    output_fifo_data_in <= filter1_out_fifo_data_out;
-                    read_flipflop := true;
-                else
-                    output_fifo_write_enable <= '0';
-                end if;
-            else
-                filter1_out_fifo_read_enable <= '0';
-                if (filter2_out_fifo_empty = '0' and output_fifo_full = '0') then
-                    filter2_out_fifo_read_enable <= '1';
-                    output_fifo_write_enable <= '1';
-                    output_fifo_data_in <= filter2_out_fifo_data_out;
-                    read_flipflop := false;
-                else
-                    output_fifo_write_enable <= '0';
-                end if;
-            end if;
-        end if;
-    end if;
-end process;
+join : entity work.join_roundrobin(Behavioral)
+    generic map (
+        FIFO_DATA_WIDTH => 32
+    )
+    port map (
+        clk => clk,
+        reset => reset,
+        input1_fifo_write_enable => filter1_out_fifo_write_enable,
+        input1_fifo_data_in => filter1_out_fifo_data_in,
+        input1_fifo_full => filter1_out_fifo_full,
+        input2_fifo_write_enable => filter2_out_fifo_write_enable,
+        input2_fifo_data_in => filter2_out_fifo_data_in,
+        input2_fifo_full => filter2_out_fifo_full,
+        output_fifo_write_enable => output_fifo_write_enable,
+        output_fifo_data_in => output_fifo_data_in,
+        output_fifo_full => output_fifo_full
+    );
+
 end Behavioral;
